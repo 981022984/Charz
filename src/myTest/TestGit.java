6efe1e6890cd143159abb9f081b1342eea277f50
zuo <981022984@qq.com>
2018-09-16 11:44:05 +0800

    zuo

--- src/myTest/TestGit.java
+++ src/myTest/TestGit.java
@@ -2,7 +2,7 @@ package myTest;
 
 public class TestGit {
 	public static void mian(String[] args) {
-		System.out.println("è¿œç¨‹ä¿®æ”¹é¡¹ç›®");
-		System.out.println("è¿œç¨‹ä¿®æ”¹é¡¹ç›®test2");
+		System.out.println("²âÊÔÔ¶³ÌĞŞ¸Ä");
+		System.out.println("²âÊÔÔ¶³ÌĞŞ¸Ätest2");
 	}
 }
