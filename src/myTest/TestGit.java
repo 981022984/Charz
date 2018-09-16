def4e73fb1284dbdc938d400b4df89660f656c83
骑士Z <40023328+981022984@users.noreply.github.com>
2018-09-16 11:08:22 +0800

    Update TestGit.java

--- src/myTest/TestGit.java
+++ src/myTest/TestGit.java
@@ -3,6 +3,6 @@ package myTest;
 public class TestGit {
 	public static void mian(String[] args) {
 		System.out.println("远程修改项目");
-		System.out.println();
+		System.out.println("远程修改项目test2");
 	}
 }
